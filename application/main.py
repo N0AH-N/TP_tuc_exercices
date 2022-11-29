cce67f52d24b1475d34b56794a25fcffc7e8f52e
N0AH-N <114983275+N0AH-N@users.noreply.github.com>
2022-11-29 11:06:16 +0100

    test

--- application/main.py
+++ application/main.py
@@ -1,3 +1,6 @@
+"""
+zoulou
+"""
 import math
 from fastapi import FastAPI
 
@@ -6,18 +9,30 @@ app = FastAPI()
 
 @app.get("/")
 def home():
+    """
+    test
+    """
     return "Hello World"
 
 
 @app.get("/{number}")
 def return_square(number: int):
+    """
+    test
+    """
     return get_square(number)
 
 
 @app.get("/twice/{number}")
 def return_square_twice(number: int):
+    """
+    test
+    """
     return get_square(number) + get_square(number)
 
 
 def get_square(number: int):
+    """
+    test
+    """
     return math.floor(math.pow(number, 2))
